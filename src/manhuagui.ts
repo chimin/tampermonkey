095ec3b444f9fb17d0f1d25c0a21b23a9df5f86c
chimin <fantazier@hotmail.com>
2021-01-22 22:29:53 +0800

    Show like show hide button

--- src/manhuagui.ts
+++ src/manhuagui.ts
@@ -12,27 +12,139 @@
 declare function GM_getValue(key: string): string;
 declare function GM_setValue(key: string, value: string): void;
 
-let notifyUpdateTimer: ReturnType<typeof setTimeout>;
+
+interface State {
+    showAll: boolean;
+    items: Record<string, 'LIKED' | 'HIDDEN'>;
+}
+
+const appStateObservers: ((state: State) => void)[] = [];
+let cachedState: State;
+let clearCachedStateRequest: ReturnType<typeof setTimeout>;
+let refreshRequest: ReturnType<typeof setTimeout>;
+
+function observeAppState(callback: (state: State) => void) {
+    appStateObservers.push(callback);
+    callback(loadAppState());
+}
+
+function updateAppState(callback: (state: State) => State) {
+    const newState = callback(loadAppState());
+    saveAppState(newState);
+    appStateObservers.forEach(callback => callback(newState));
+}
+
+function refresh() {
+    clearTimeout(refreshRequest);
+    refreshRequest = setTimeout(() => window.dispatchEvent(new Event('scroll')), 500);
+}
+
+function loadAppState(): State {
+    return cachedState || parseJson<State>(localStorage.getItem('__appState'));
+}
+
+function saveAppState(state: State) {
+    localStorage.setItem('__appState', JSON.stringify(state));
+    cachedState = state;
+    clearTimeout(clearCachedStateRequest);
+    clearCachedStateRequest = setTimeout(() => cachedState = null, 100);
+}
+
+function like(title: string) {
+    updateAppState(state => ({ ...state, items: { ...state.items, [title]: 'LIKED' } }));
+}
+
+function show(title: string) {
+    updateAppState(state => ({ ...state, items: { ...state.items, [title]: undefined } }));
+}
+
+function hide(title: string) {
+    updateAppState(state => {
+        if (state?.items?.[title] == 'LIKED' && !confirm(`Are you sure you want to hide ${title}?`)) {
+            return state;
+        }
+        return { ...state, items: { ...state.items, [title]: 'HIDDEN' } };
+    });
+}
+
+function showAll() {
+    updateAppState(state => ({ ...state, showAll: true }));
+}
+
+function hideUnwanted() {
+    updateAppState(state => ({ ...state, showAll: undefined }));
+}
+
 
 document.querySelectorAll<HTMLLIElement>('.latest-cont .latest-list li')
-    .forEach(li => {
-        const title = li.querySelector<HTMLAnchorElement>('a[title]').title;
-        li.style.position = 'relative';
-        li.appendChild(createCloseButton(li, title));
+    .forEach(li => processItem(li));
+document.body.appendChild(createToolbar());
+observeAppState(() => refresh());
+
+function processItem(li: HTMLLIElement) {
+    const title = li.querySelector<HTMLAnchorElement>('a[title]').title;
+    li.style.position = 'relative';
+    li.appendChild(createItemToolbar(title));
+
+    observeAppState(state => {
+        const isHidden = state?.items?.[title] == 'HIDDEN';
+        const show = state?.showAll || !isHidden;
+        li.style.opacity = isHidden ? '.5' : '';
+        li.style.display = show ? '' : 'none';
+    });
+}
+
+function createItemToolbar(title: string) {
+    const div = document.createElement('div');
+    div.style.position = 'absolute';
+    div.style.right = '28px';
+    div.style.top = '0';
+    div.style.zIndex = '99999';
+    div.appendChild(createLikeButton(title));
+    div.appendChild(createShowButton(title));
+    div.appendChild(createHideButton(title));
+    return div;
+}
+
+function createLikeButton(title: string) {
+    const button = document.createElement('button');
+    button.textContent = 'Like';
+    button.style.padding = '1px 5px';
+    button.onclick = () => like(title);
+
+    observeAppState(state => {
+        const show = state?.items?.[title] != 'LIKED';
+        button.style.display = show ? '' : 'none';
     });
 
-document.body.appendChild(createToolbar());
-update();
+    return button;
+}
 
-function createCloseButton(li: HTMLLIElement, title: string) {
+function createShowButton(title: string) {
     const button = document.createElement('button');
-    button.textContent = 'X';
+    button.textContent = 'Show';
     button.style.padding = '1px 5px';
-    button.style.position = 'absolute';
-    button.style.top = '0';
-    button.style.right = '28px';
-    button.style.zIndex = '99999';
-    button.onclick = e => getHideList().includes(title) ? show(title) : hide(title);
+    button.onclick = () => show(title);
+
+    observeAppState(state => {
+        const show = state?.items?.[title] == 'HIDDEN';
+        button.style.display = show ? '' : 'none';
+    });
+
+    return button;
+}
+
+function createHideButton(title: string) {
+    const button = document.createElement('button');
+    button.textContent = 'Hide';
+    button.style.padding = '1px 5px';
+    button.onclick = () => hide(title);
+
+    observeAppState(state => {
+        const show = state?.items?.[title] != 'HIDDEN';
+        button.style.display = show ? '' : 'none';
+    });
+
     return button;
 }
 
@@ -44,69 +156,45 @@ function createToolbar() {
     div.style.right = '10px';
     div.style.bottom = '10px';
     div.style.zIndex = '999999';
-    div.appendChild(createToggleButton());
+    div.appendChild(createShowAllButton());
+    div.appendChild(createHideUnwantedButton());
     return div;
 }
 
-function createToggleButton() {
+function createShowAllButton() {
     const button = document.createElement('button');
-    button.textContent = 'Toggle';
+    button.textContent = 'Show all';
     button.style.padding = '1px 5px';
-    button.onclick = () => toggleHide();
+    button.onclick = () => showAll();
+
+    observeAppState(state => {
+        const show = !state?.showAll;
+        button.style.display = show ? '' : 'none';
+    });
+
     return button;
 }
 
-function shouldHide() {
-    return (GM_getValue('shouldHide') || '1') == '1';
+function createHideUnwantedButton() {
+    const button = document.createElement('button');
+    button.textContent = 'Hide unwanted';
+    button.style.padding = '1px 5px';
+    button.onclick = () => hideUnwanted();
+
+    observeAppState(state => {
+        const show = state?.showAll;
+        button.style.display = show ? '' : 'none';
+    });
+
+    return button;
 }
 
-function toggleHide() {
-    GM_setValue('shouldHide', !shouldHide() ? '1' : '0');
-    update();
-}
-
-function getHideList() {
-    return parseJson<string[]>(GM_getValue('hideList')) || [];
-}
-
-function hide(title: string) {
-    let list = getHideList();
-    list = list.filter(a => a != title).concat(title);
-    GM_setValue('hideList', JSON.stringify(list));
-    update();
-}
-
-function show(title: string) {
-    let list = getHideList();
-    list = list.filter(a => a != title);
-    GM_setValue('hideList', JSON.stringify(list));
-    update();
-}
-
-function clear() {
-    localStorage.removeItem('hideList');
-    update();
-}
-
-function update() {
-    const hideList = getHideList();
-    const nowShouldHide = shouldHide();
-    document.querySelectorAll<HTMLLIElement>('.latest-cont .latest-list li')
-        .forEach(li => {
-            const title = li.querySelector<HTMLAnchorElement>('a[title]').title;
-            if (nowShouldHide && hideList.includes(title)) {
-                li.style.display = 'none';
-            } else {
-                li.style.display = '';
-                li.style.opacity = hideList.includes(title) ? '.3' : '';
-            }
-
-            clearTimeout(notifyUpdateTimer);
-            notifyUpdateTimer = setTimeout(() => window.dispatchEvent(new Event('scroll')), 500);
-        });
-}
 
 function parseJson<T>(str: string): T {
+    if (!str) {
+        return null;
+    }
+
     try {
         return JSON.parse(str);
     } catch (e) {
